f606d80979df8b44e1efd007cba100f1112e4e12
MohammedAydan <66109806+MohammedAydan@users.noreply.github.com>
2022-12-18 13:15:37 +0200

    Add files via upload

---
+++ Perfect Number/Perfect Number/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perfect_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter start number : ");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter stop number : ");
+            int n2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("..........");
+
+            for (int i=n1; i < n2; i++)
+            {
+                int sum = 0;
+                for(int j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        sum = sum + j;
+                    }
+                }
+                if(sum == i)
+                {
+                    Console.WriteLine(".  "+sum);
+                }
+            }
+            Console.WriteLine("..........");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Prime Numbers/Prime Numbers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter strart number : ");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter stop number : ");
+            int n2 = int.Parse(Console.ReadLine());
+
+            int j, i;
+
+            Console.WriteLine("...........");
+
+            for (i = n1; i <= n2; i++)
+            {
+                for (j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        break;
+                    }
+                }
+                if (i == j)
+                {
+                    Console.WriteLine(".  " + i);
+                }
+            }
+            Console.WriteLine("...........");
+
+            Console.ReadKey();
+        }
+    }
+}
